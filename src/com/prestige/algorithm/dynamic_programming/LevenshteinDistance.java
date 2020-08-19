34a5fe3006a51359215fda55b675e9ae274dee48
Godson Ositadinma <prestigegodson@gmail.com>
2020-08-19 01:24:21 +0100

    updated levenshtein distance algorithm

--- src/com/prestige/algorithm/dynamic_programming/LevenshteinDistance.java
+++ src/com/prestige/algorithm/dynamic_programming/LevenshteinDistance.java
@@ -4,75 +4,36 @@ public class LevenshteinDistance {
 
     private final String str1;
     private final String str2;
-
-    private int rowLength;
-    private int columnLength;
     private Integer[][] matrix;
 
     public LevenshteinDistance(String str1, String str2) {
         this.str1 = str1;
         this.str2 = str2;
-
-        rowLength = str1.length();
-        columnLength = str2.length();
-
-        matrix = new Integer[rowLength+1][columnLength+1];
+        matrix = new Integer[str1.length()+1][str2.length()+1];
     }
 
-    private Integer getDistance(){
-        matrix[0][0] = 0;
+    public Integer getDistance(){
 
-        int row = 0;
-        int column = 1;
-        matrix[row][column] = 0;
-
-        while(column <= columnLength){
-            matrix[0][column] =  matrix[0][column - 1] + 1;
-            column++;
+        for(int i=0; i<str1.length() + 1; i++){
+            for(int j=0; j<str2.length() + 1; j++){
+                matrix[i][j] = j;
+            }
+            matrix[i][0] = i;
         }
 
-        row = 1;
-
-        while(row <= rowLength){
-            matrix[row][0] = matrix[row - 1][0] + 1;
-            row++;
-        }
-
-        row = 1;
-        column = 1;
-
-        while(row <= rowLength){
-
-            while(column <= columnLength){
-                if(str1.charAt(row-1) == str2.charAt(column-1)){
-                    matrix[row][column] = matrix[row-1][column-1];
+        for(int row=1; row<str1.length() + 1; row++){
+            for(int col=1; col<str2.length() + 1; col++){
+                if(str1.charAt(row-1) == str2.charAt(col-1)){
+                    matrix[row][col] = matrix[row-1][col-1];
                 }else{
-                    matrix[row][column] = Math.min(Math.min(matrix[row][column-1],matrix[row-1][column-1]),
-                            matrix[row-1][column]) + 1;
+                    matrix[row][col] = Math.min(Math.min(matrix[row][col-1],matrix[row-1][col-1]),
+                            matrix[row-1][col]) + 1;
                 }
-                column++;
             }
-            column = 1;
-            row++;
         }
 
-        for(int i=0; i<matrix.length;i++){
-            for(int j=0; j<matrix[i].length;j++){
-                if(j == 0) {
-                    System.out.print(matrix[i][j]);
-                    continue;
-                }
-
-                System.out.print("," + matrix[i][j]);
-
-            }
-            System.out.println("");
-        }
-
-        return matrix[rowLength][columnLength];
+        return matrix[str1.length()][str2.length()];
     }
-
-
 
     public static void main(String[] args) {
 
